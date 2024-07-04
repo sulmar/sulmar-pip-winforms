2ceb3bfee360f1768d9bda9b0bb369c710d16c18
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-07-04 15:03:53 +0200

    Dodanie powłoki

---
+++ src/WinFormsApp/Views/ShellView.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WinFormsApp.Views
+{
+    partial class ShellView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            PanelToolbar = new Panel();
+            ShowInspectorsButton = new Button();
+            ShowDocumentsButton = new Button();
+            PanelToolbar.SuspendLayout();
+            SuspendLayout();
+            // 
+            // PanelToolbar
+            // 
+            PanelToolbar.Controls.Add(ShowDocumentsButton);
+            PanelToolbar.Controls.Add(ShowInspectorsButton);
+            PanelToolbar.Dock = DockStyle.Top;
+            PanelToolbar.Location = new Point(0, 0);
+            PanelToolbar.Name = "PanelToolbar";
+            PanelToolbar.Size = new Size(800, 100);
+            PanelToolbar.TabIndex = 0;
+            // 
+            // ShowInspectorsButton
+            // 
+            ShowInspectorsButton.Location = new Point(41, 24);
+            ShowInspectorsButton.Name = "ShowInspectorsButton";
+            ShowInspectorsButton.Size = new Size(129, 57);
+            ShowInspectorsButton.TabIndex = 0;
+            ShowInspectorsButton.Text = "Inspektorzy";
+            ShowInspectorsButton.UseVisualStyleBackColor = true;
+            ShowInspectorsButton.Click += ShowInspectorsButton_Click;
+            // 
+            // ShowDocumentsButton
+            // 
+            ShowDocumentsButton.Location = new Point(189, 24);
+            ShowDocumentsButton.Name = "ShowDocumentsButton";
+            ShowDocumentsButton.Size = new Size(129, 57);
+            ShowDocumentsButton.TabIndex = 1;
+            ShowDocumentsButton.Text = "Dokumenty";
+            ShowDocumentsButton.UseVisualStyleBackColor = true;
+            ShowDocumentsButton.Click += ShowDocumentsButton_Click;
+            // 
+            // ShellView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(PanelToolbar);
+            Name = "ShellView";
+            Text = "ShellView";
+            PanelToolbar.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel PanelToolbar;
+        private Button ShowDocumentsButton;
+        private Button ShowInspectorsButton;
+    }
+}
---
+++ src/WinFormsApp/Views/ShellView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp.Views
+{
+    public partial class ShellView : Form
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowInspectorsButton_Click(object sender, EventArgs e)
+        {
+            var view = new InspectorsView();
+            view.Show();
+
+        }
+
+        private void ShowDocumentsButton_Click(object sender, EventArgs e)
+        {
+            var view = new DocumentsByUsersView();
+            view.Show();
+        }
+    }
+}
