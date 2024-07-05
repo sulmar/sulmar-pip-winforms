8bd602bc486163ac937fd9beb79527fad7e3ee36
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-07-05 15:32:12 +0200

    Dodanie AD

--- src/WinFormsApp/Views/ShellView.cs
+++ src/WinFormsApp/Views/ShellView.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.DirectoryServices.AccountManagement;
 using System.Drawing;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -28,6 +31,41 @@ namespace WinFormsApp.Views
         {
             var view = new DocumentsByUsersView();
             view.Show();
+
+            string secretKey = Environment.GetEnvironmentVariable("MY_SECRET_KEY");
+
+            // var result = Microsoft.Win32.Registry.GetValue("a", "b", 0);
+
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+
+            var username = identity.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name).Value;
+
+            // var email = identity.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email).Value;
+
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+
+            if (principal.IsInRole("trainer"))
+            {
+
+            }
+
+            // dotnet add package System.DirectoryServices.AccountManagement
+            PrincipalContext context = new PrincipalContext(ContextType.Machine);
+
+            UserPrincipal user = UserPrincipal.FindByIdentity(context, username);
+
+            var name = user.DisplayName;
+
+            GroupPrincipal group = GroupPrincipal.FindByIdentity(context, IdentityType.Name, "Administratorzy");
+
+            if (group != null)
+            {
+                foreach (Principal p in group.GetMembers(true))
+                {
+                    Console.WriteLine(p.Name);
+                }
+            }
+
         }
     }
 }
